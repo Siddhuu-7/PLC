0c58b55c2562da5cefa1eb4a6de8030499496866
Siddhardha Utla <siddhardhautla19@gmail.com>
2026-01-08 16:12:56 +0530

    add cloud mysql

--- SQLdb.config.js
+++ SQLdb.config.js
@@ -1,9 +1,27 @@
 import { Sequelize } from "sequelize";
 import dotenv from 'dotenv'
 dotenv.config()
-const sequelize = new Sequelize('PLC', 'root', '23B91A12H3', {
-  host: 'localhost',
-  dialect:  'mysql' 
-});
+import fs from "fs";
+
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT, // e.g. 25060
+    dialect: "mysql",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        ca: process.env.NODE_ENV==="production"?fs.readFileSync("./etc/secrets/ca.pem"):fs.readFileSync("./ca.pem"),
+        rejectUnauthorized: true
+      },
+      connectTimeout: 20000
+    }
+  }
+);
 
 export default sequelize;
+
